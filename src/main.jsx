d2857eab97ba25853ac573ee494363dc9f95dac0
ss23094527 <ss23094527@gmail.com>
2023-02-22 12:47:07 +0800

    NAVBAR變更

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './bootstrap.min.css'
 import './index.css'
+import './bootstrap.bundle.min.js'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    
+   
+  
   </React.StrictMode>,
 )
 
